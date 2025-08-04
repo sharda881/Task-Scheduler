c072996ca31539a07215872adbf2622c255bf04e
Sharda Nand Jha <shardanand.jha@walmart.com>
2025-08-04 17:17:48 +0530

    Added code to test-db connection

--- src/main/java/com/alpha/task_scheduler/TaskSchedulerApplication.java
+++ src/main/java/com/alpha/task_scheduler/TaskSchedulerApplication.java
@@ -1,13 +1,26 @@
 package com.alpha.task_scheduler;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TaskSchedulerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TaskSchedulerApplication.class, args);
+	}
+
+
+	@Bean
+	public CommandLineRunner demo(TaskRepository repository) {
+		return args -> {
+			Task task = new Task();
+			task.setTitle("Test PostgreSQL Task");
+			repository.save(task);
+			System.out.println("✅ Task saved to database!");
+		};
 	}
 
 }
